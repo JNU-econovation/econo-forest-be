6cc310f78647f5e8c1bfd4ccd08a1b73d33c2988
bellljun3395 <195850@jnu.ac.kr>
2022-11-24 08:19:41 +0900

    feat: econo-forest 초기 설정

---
+++ src/main/java/com/example/econoforestbe/EconoForestBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.econoforestbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconoForestBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconoForestBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/econoforestbe/EconoForestBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.econoforestbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconoForestBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
