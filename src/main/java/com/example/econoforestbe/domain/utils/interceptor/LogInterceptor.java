ed24cca284e049c45bb240612b5e467e57193812
bellljun3395 <195850@jnu.ac.kr>
2022-11-27 15:28:02 +0900

    feat: preHandle 구현

---
+++ src/main/java/com/example/econoforestbe/domain/utils/interceptor/LogInterceptor.java
@@ -0,0 +1,32 @@
+package com.example.econoforestbe.domain.utils.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    public static final String LOG_ID = "logId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID()
+                .toString();
+
+        request.setAttribute(LOG_ID, uuid);
+
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod hm = (HandlerMethod) handler;
+        }
+
+        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+
+        return true;
+    }
+}
