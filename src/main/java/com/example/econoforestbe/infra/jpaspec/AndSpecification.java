115cb76eb24ca1f1ee7888b7a18de2d76b4dd003
bellljun3395 <195850@jnu.ac.kr>
2022-11-28 23:20:40 +0900

    feat: AndSpecification 구현

---
+++ src/main/java/com/example/econoforestbe/infra/jpaspec/AndSpecification.java
@@ -0,0 +1,27 @@
+package com.example.econoforestbe.infra.jpaspec;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.Arrays;
+import java.util.List;
+
+public class AndSpecification<T> implements Specification<T> {
+
+    private List<Specification<T>> specs;
+
+    public AndSpecification(Specification<T> ...specs) {
+        this.specs = Arrays.asList(specs);
+    }
+
+    @Override
+    public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+        Predicate[] predicates = specs.stream()
+                .map(spec -> spec.toPredicate(root, query, criteriaBuilder))
+                .toArray(size -> new Predicate[size]);
+        return criteriaBuilder.and(predicates);
+    }
+}
