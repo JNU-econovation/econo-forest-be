00b559a7c6d52b91ca0fce4e374b8af11f6b1a06
bellljun3395 <195850@jnu.ac.kr>
2022-11-28 23:16:32 +0900

    feat: EatTogatherMember 구현

---
+++ src/main/java/com/example/econoforestbe/domain/eatToagther/EatTogatherMember.java
@@ -0,0 +1,19 @@
+package com.example.econoforestbe.domain.eatToagther;
+
+import com.example.econoforestbe.domain.Member.Member;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@NoArgsConstructor
+public class EatTogatherMember {
+    @Column(name = "eating_memberId")
+    private Long memberId;
+
+    public EatTogatherMember(Member member) {
+        this.memberId = member.getId();
+    }
+
+}
