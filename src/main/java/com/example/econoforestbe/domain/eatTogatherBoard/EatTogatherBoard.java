24cf521861038e120b5bed0ce6465848e1eec772
bellljun3395 <195850@jnu.ac.kr>
2022-12-02 00:11:29 +0900

    feat(EatTogatherBoard): EatTogatherBoard 구현

---
+++ src/main/java/com/example/econoforestbe/domain/eatTogatherBoard/EatTogatherBoard.java
@@ -0,0 +1,22 @@
+package com.example.econoforestbe.domain.eatTogatherBoard;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class EatTogatherBoard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private LocalDateTime dateTime;
+
+    private String location;
+
+}
