fb45b60e09ca42352f7c67ec7c6ba3be44c52a6f
bellljun3395 <195850@jnu.ac.kr>
2022-12-02 17:00:54 +0900

    feat(JoinMember): JoinMember 구현

---
+++ src/main/java/com/example/econoforestbe/domain/eatTogatherBoard/JoinMember.java
@@ -0,0 +1,14 @@
+package com.example.econoforestbe.domain.eatTogatherBoard;
+
+public class JoinMember {
+
+    private final Long memberId;
+
+    public JoinMember(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+}
