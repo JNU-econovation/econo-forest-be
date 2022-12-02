02321399af8c522a253057f1af1b45249bb5f960
bellljun3395 <195850@jnu.ac.kr>
2022-12-02 21:59:09 +0900

    feat(EatTogatherService): EatTogatherService 구현
    - postEatTogather
    - readEatTogather

---
+++ src/main/java/com/example/econoforestbe/service/eatTogatherBoard/EatTogatherService.java
@@ -0,0 +1,30 @@
+package com.example.econoforestbe.service.eatTogatherBoard;
+
+import com.example.econoforestbe.domain.eatTogatherBoard.EatTogather;
+import com.example.econoforestbe.domain.eatTogatherBoard.EatTogatherRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class EatTogatherService {
+
+    private EatTogatherRepository eatTogatherRepository;
+
+    @Transactional
+    public void postEatTogather(EatTogather eatTogather) {
+        EatTogather savedEatTogather = eatTogatherRepository.save(eatTogather);
+    }
+
+    public EatTogather readEatTogather(Long eatTogatherId) {
+        Optional<EatTogather> byIdEatTogather = eatTogatherRepository.findById(eatTogatherId);
+        if (byIdEatTogather.isEmpty()) {
+            throw new IllegalArgumentException("there are no eatTogather record");
+        }
+        return byIdEatTogather.get();
+    }
+}
