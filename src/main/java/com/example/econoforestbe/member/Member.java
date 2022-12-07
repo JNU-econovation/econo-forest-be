7361a09dee20389662200f484822870ed990306f
kssumin <201566@jnu.ac.kr>
2022-12-07 14:46:36 +0900

    feat : member domain 생성

---
+++ src/main/java/com/example/econoforestbe/member/Member.java
@@ -0,0 +1,20 @@
+package com.example.econoforestbe.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long ipdId;
+}
