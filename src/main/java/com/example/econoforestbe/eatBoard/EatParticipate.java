b0ed1bbf47a5ee3b05b886b122abadb8e664db44
kssumin <201566@jnu.ac.kr>
2022-12-08 12:53:43 +0900

    refactor : 파라미터 전부 가지고 있는 생성자 추가

--- src/main/java/com/example/econoforestbe/eatBoard/EatParticipate.java
+++ src/main/java/com/example/econoforestbe/eatBoard/EatParticipate.java
@@ -1,10 +1,12 @@
 package com.example.econoforestbe.eatBoard;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class EatParticipate {
     private Long idpId;
 }
--- src/main/java/com/example/econoforestbe/join/JoinMember.java
+++ src/main/java/com/example/econoforestbe/join/JoinMember.java
@@ -1,8 +1,14 @@
 package com.example.econoforestbe.join;
 
+import com.example.econoforestbe.eatBoard.EatParticipate;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
 public class JoinMember {
     private Long idpId;
+
+    public EatParticipate convertToEatParticipate() {
+        return new EatParticipate(idpId);
+    }
 }
