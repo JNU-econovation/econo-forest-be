8dce4622121ec4afea19e7e6c2d50643a41aa1d5
kssumin <201566@jnu.ac.kr>
2022-12-08 11:53:29 +0900

    feat : 이미 참여한 사람인지 조회하기 위해 추상클래스에만 의존

--- src/main/java/com/example/econoforestbe/eatBoard/EatMember.java
+++ src/main/java/com/example/econoforestbe/eatBoard/EatMember.java
@@ -3,6 +3,7 @@ package com.example.econoforestbe.eatBoard;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class EatMember {
@@ -10,4 +11,21 @@ public class EatMember {
     @GeneratedValue
     private Long id;
     private Long idpId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EatMember)) return false;
+        EatMember eatMember = (EatMember) o;
+        return idpId.equals(eatMember.idpId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idpId);
+    }
+
+    public boolean isSatisfiedBy(EatParticipate eatParticipate) {
+        return Objects.equals(idpId,eatParticipate.getIdpId());
+    }
 }
---
+++ src/main/java/com/example/econoforestbe/eatBoard/EatParticipate.java
@@ -0,0 +1,10 @@
+package com.example.econoforestbe.eatBoard;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class EatParticipate {
+    private Long idpId;
+}
