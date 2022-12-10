5830c76581e2977ca3cb8a0ce2cdabb4c141e41b
kssumin <201566@jnu.ac.kr>
2022-12-10 11:37:07 +0900

    feat : 밥 먹어요에 참여할 때 전송받는 데이터 생성

--- src/main/java/com/example/econoforestbe/web/dto/JoinEatDto.java
+++ src/main/java/com/example/econoforestbe/web/dto/JoinEatDto.java
@@ -1,2 +1,16 @@
-package com.example.econoforestbe.web.dto;public class JoinEatDto {
+package com.example.econoforestbe.web.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@Builder
+public class JoinEatDto {
+    private LocalDate eatDateWhenJoin;
+    private LocalTime eatTimeWhenJoin;
+    private Long eatBoardId;
+    private Long joinMemberId;
 }
