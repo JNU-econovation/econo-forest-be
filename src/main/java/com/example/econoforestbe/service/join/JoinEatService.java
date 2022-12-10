57aa0cbc76262d1dbf934899ec4b7606e6957d6c
kssumin <201566@jnu.ac.kr>
2022-12-10 12:07:42 +0900

    feat(JoinEatService) : 특정 밥 먹어요에 참여 service단 구현

--- src/main/java/com/example/econoforestbe/service/join/JoinEatService.java
+++ src/main/java/com/example/econoforestbe/service/join/JoinEatService.java
@@ -1,2 +1,22 @@
-package com.example.econoforestbe.service.join;public class JoinEatService {
+package com.example.econoforestbe.service.join;
+
+import com.example.econoforestbe.domain.join.JoinEat;
+import com.example.econoforestbe.web.dto.JoinEatDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class JoinEatService {
+    private final JoinEatMapper joinEatMapper;
+
+    @Transactional
+    public boolean joinEatTogather(JoinEatDto joinEatDto) {
+        JoinEat joinEat = joinEatMapper.mapFrom(joinEatDto);
+        return joinEat.joinEatBoard();
+    }
+
 }
