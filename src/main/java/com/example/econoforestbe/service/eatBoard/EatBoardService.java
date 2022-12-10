c5e396374693b6771a6023ee7d86f3bcb6eb910e
kssumin <201566@jnu.ac.kr>
2022-12-10 10:58:04 +0900

    feat(EatBoardService) : 밥 먹어요 글 불러오기

---
+++ src/main/java/com/example/econoforestbe/service/eatBoard/EatBoardService.java
@@ -0,0 +1,27 @@
+package com.example.econoforestbe.service.eatBoard;
+
+import com.example.econoforestbe.domain.eatBoard.EatBoard;
+import com.example.econoforestbe.domain.eatBoard.EatBoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class EatBoardService {
+    private static final String NOT_FOUND_EAT_BOARD="해당 하는 밥먹어요 글이 없습니다";
+
+    private final EatBoardRepository eatBoardRepository;
+
+
+    public EatBoard getEatBoard(Long eatBoardId) {
+        Optional<EatBoard> eatBoard=eatBoardRepository.findById(eatBoardId);
+        if (eatBoard.isEmpty()) {
+            throw new IllegalArgumentException(NOT_FOUND_EAT_BOARD);
+        }
+        return eatBoard.get();
+    }
+}
