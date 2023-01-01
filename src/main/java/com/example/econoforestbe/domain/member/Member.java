90746df11e221f80b526dcac0ccbb67a21f2515a
kssumin <201566@jnu.ac.kr>
2023-01-01 20:52:27 +0900

    feat(MemberRepository) : MemberRepository 생성

--- src/main/java/com/example/econoforestbe/domain/member/Member.java
+++ src/main/java/com/example/econoforestbe/domain/member/Member.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -12,9 +13,14 @@ import javax.persistence.Id;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+/**
+ * 일반회원은 idp server에서 accessToken으로 정보를 가져오지만, 관리자의 경우에는 우리 서버에서 따로 관리해야 한다.
+ * 따라서 이 클래스는 관리자를 관리하기 위한 용도
+ */
 public class Member {
     @Id
     @GeneratedValue
+    @Column(name="MEMBER_ID")
     private Long id;
     private Long ipdId;
 }
