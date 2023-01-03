85654002f79f2fb27cbe5c325dc8eb45f4582528
kssumin <201566@jnu.ac.kr>
2023-01-03 15:44:10 +0900

    feat : 생성일,수정일,삭제일 Audit 이용하여 구현

--- src/main/java/com/example/econoforestbe/domain/eatBoard/EatBoard.java
+++ src/main/java/com/example/econoforestbe/domain/eatBoard/EatBoard.java
@@ -1,9 +1,12 @@
 package com.example.econoforestbe.domain.eatBoard;
 
+import com.example.econoforestbe.domain.Timestamped;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 
@@ -12,7 +15,9 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class EatBoard {
+@Where(clause = "deleted_at IS NULL")
+@SQLDelete(sql="UPDATE eat_board SET deleted_at=CURRENT_DATE where eat_board_id=?")
+public class EatBoard extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "EAT_BOARD_ID")
@@ -24,13 +29,8 @@ public class EatBoard {
     @Column(name = "EAT_BOARD_LOCATION_CATEGORY")
     private LocationCategory locationCategory;
     @Embedded
-    private EatInfo eatInfo;
-    @Embedded
     private EatMembers eatMembers;
+    @Embedded
+    private EatInfo eatInfo;
 
-    private boolean isDeleted;
-
-    public void delete() {
-        this.isDeleted = true;
-    }
 }
