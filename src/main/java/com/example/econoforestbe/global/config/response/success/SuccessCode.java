890ab0819287b19d4bee854e2b15918ca8b8c6d9
kssumin <201566@jnu.ac.kr>
2023-01-24 14:59:41 +0900

    refactor : 글 참여/불참 성공 응답 형식 맞춤

--- src/main/java/com/example/econoforestbe/global/config/response/success/SuccessCode.java
+++ src/main/java/com/example/econoforestbe/global/config/response/success/SuccessCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode {
 
     CREATED_EAT_BOARD(HttpStatus.CREATED, "200100", "밥 먹어요 글이 생성되었습니다."),
-    EDIT_EAT_BOARD(HttpStatus.OK, "200101", "밥 먹어요 글 수정 완료 했습니다.");
+    EDIT_EAT_BOARD(HttpStatus.OK, "200101", "밥 먹어요 글 수정 완료 했습니다."),
+    PARTICIPATE_EAT_BOARD(HttpStatus.OK, "200102", "밥 먹어요 글에 참여했습니다."),
+    NON_PARTICIPATE_EAT_BOARD(HttpStatus.OK, "200103", "밥 먹어요 글에 불참했습니다.");
     SuccessCode(HttpStatus httpStatus, String errorCode, String message) {
         this.httpStatus = httpStatus;
         this.errorCode = errorCode;
