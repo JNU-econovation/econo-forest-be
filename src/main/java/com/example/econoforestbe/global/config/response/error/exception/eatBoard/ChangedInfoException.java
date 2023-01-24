299f9e103cb9a9c5d00875dc79fd1cea2eb95cb8
kssumin <201566@jnu.ac.kr>
2023-01-24 21:59:52 +0900

    feat : 참여 시 글이 변경되었다는 에러 생성

--- src/main/java/com/example/econoforestbe/global/config/response/error/exception/eatBoard/ChangedInfoException.java
+++ src/main/java/com/example/econoforestbe/global/config/response/error/exception/eatBoard/ChangedInfoException.java
@@ -1,2 +1,10 @@
-package com.example.econoforestbe.global.config.response.error.exception.eatBoard;public class ChangedInfo {
+package com.example.econoforestbe.global.config.response.error.exception.eatBoard;
+
+import com.example.econoforestbe.global.config.response.error.ErrorCode;
+import com.example.econoforestbe.global.config.response.error.exception.BusinessException;
+
+public class ChangedInfoException extends BusinessException {
+    public ChangedInfoException(){
+        super(ErrorCode.CHANGED_INFO);
+    }
 }
